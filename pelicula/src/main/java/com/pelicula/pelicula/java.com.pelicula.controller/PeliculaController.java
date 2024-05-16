440dbb3cd13ab3c03361470140b763314497917f
feernandobaena <baenarosello.fernando@digitechfp.com>
2024-05-16 11:08:34 +0200

    cambios package

--- pelicula/src/main/java/com/pelicula/pelicula/java.com.pelicula.controller/PeliculaController.java
+++ pelicula/src/main/java/com/pelicula/pelicula/java.com.pelicula.controller/PeliculaController.java
@@ -1,16 +1,21 @@
+package com.pelicula.pelicula.java.com.pelicula;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pelicula.pelicula.java.com.pelicula.pelicula.repositories.usuarioRepository;
 import com.pelicula.pelicula.java.com.pelicula.pelicula.models.pelicula;
+import com.pelicula.pelicula.java.com.pelicula.pelicula.models.usuario;
+import com.pelicula.pelicula.java.com.pelicula.pelicula.repositories.peliculaRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/pelicula/")
-public class peliculaController {
+public class PeliculaController {
     @Autowired
     private peliculaRepository peliRepo;
     
@@ -20,9 +25,9 @@ public class peliculaController {
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<usuario> getPersonaById(@PathVariable Long id) {
-        Optional<pelicula> persona = peliRepo.findById(id);
-        return persona.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<pelicula> getPersonaById(@PathVariable Long id) {
+        Optional<pelicula> pelicula = peliRepo.findById(id);
+        return pelicula.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
     
-}
+} 
--- pelicula/src/main/java/com/pelicula/pelicula/java.com.pelicula.pelicula.repositories/usuarioRepository.java
+++ pelicula/src/main/java/com/pelicula/pelicula/java.com.pelicula.pelicula.repositories/usuarioRepository.java
@@ -1,4 +1,6 @@
-package com.pelicula.pelicula.java.com.pelicula.pelicula.repositories;
+package com.pelicula.pelicula.java.com.pelicula.pelicula;
+
+
 import com.pelicula.pelicula.java.com.pelicula.pelicula.models.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
