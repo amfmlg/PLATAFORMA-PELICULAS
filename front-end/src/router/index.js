a50663e0be8b7152347378f2bf33deed6151b967
feernandobaena <baenarosello.fernando@digitechfp.com>
2024-05-20 12:01:46 +0200

    Inicio de proyecto de vue

---
+++ front-end/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ExampleView from '../views/ExampleView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/example',
+      name: 'example',
+      component: ExampleView
+    },
+   
+  ]
+})
+
+export default router
---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import router from '@/router';
+
+const goto = () => {
+  router.push('/example')
+}
+</script>
+
+<template>
+<h1>Hola mundo</h1>
+<button @click="goto">example</button>
+</template>
