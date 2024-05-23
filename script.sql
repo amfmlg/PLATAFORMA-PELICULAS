cb1b216a9630e0dc948b8bb702e56f12492c362d
AMF <148863482+amfmlg@users.noreply.github.com>
2024-05-23 09:32:00 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -5,7 +5,8 @@ CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `usuario` varchar(255) NOT NULL,
   `correo` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL
+  `contraseña` varchar(255) NOT NULL,
+  `fechaNacimiento` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -18,6 +19,15 @@ CREATE TABLE `pelicula` (
   `descripcion` varchar(255) NOT NULL,
   `fechaLanzamiento` date NOT NULL,
   `duracion` int(11) NOT NULL,
-  `valoracion` float NOT NULL
+  `valoracion` float NOT NULL,
+  `trailer` varchar(255) NOT NULL,
+  `dondeVer` varchar(255) NOT NULL,
+  `idioma` varchar(255) NOT NULL,
+  `edadRecomendada` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `contenido` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
